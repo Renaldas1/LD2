e32d72a932387bf45335fac80cbc27e058aacdf0
renbud <renbud@C406A7>
2022-03-14 08:56:58 +0200

    Add project files.

---
+++ LD2/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LD2
+{
+    internal class Class1
+    {
+
+        public void MyMethod()
+        {
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Thread.Sleep(5000);
+
+            double a = 1;
+            double b = 2;
+            double c = 0;
+
+            while (c <= Math.Pow(10, 15))
+            {
+                c = a * b;
+                b = a;
+                a = c;
+
+            }
+            Console.WriteLine("Paskutine reiksme: " + c);
+            stopwatch.Stop();
+
+            TimeSpan ts = stopwatch.Elapsed;
+
+            Console.WriteLine("Elapsed Time is {0:00}:{1:00}:{2:00}.{3}",
+            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+        }
+        public void SekosNr()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 0;
+            int d = 0;
+
+            while (c <= Math.Pow(10, 15))
+            {
+                c = a * b;
+                b = a;
+                a = c;
+                d = d + 1;
+            }
+            Console.WriteLine("Sekos nr: " + d);
+        }
+
+    }
+
+}
+    
+
+
---
+++ LD2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//nebūtina
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+//nebūtina
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LD2.Class1 myObj = new LD2.Class1();
+            LD2.Class1 myObj2 = new LD2.Class1();
+            myObj.MyMethod();
+            myObj2.SekosNr();
+
+        }
+    }
+}
+
